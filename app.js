1d2f53298de53c6a90d704a1180a7c7419b0a7a2
Rapafull <rapafull@gmail.com>
2019-01-25 15:33:22 -0600

    commit 2

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ require('./db/connect')
 
 //public
 app.use(express.static(path.resolve(__dirname, './public')));
+app.use(express.static(path.resolve(__dirname, './upload/rr@gmail.com')));
 
 
 app.listen(process.env.PORT, (err => {
